7bb73462b58d068cdb16f03d1960df2bb7a23769
mosart <maurice.vanderfeesten@gmail.com>
2016-07-12 15:58:15 +0200

    Added plots showing tools with all disciplines grouped

--- LaadVariabelen.R
+++ LaadVariabelen.R
@@ -11,4 +11,8 @@ Disciplines<- c("ArtsHumanities","EngineeringTechnology","Law","LifeSciences","M
 roles<- c("PhD student","Postdoc","Professor / Associate professor / Assistant professor","Librarian")
 
 Kleuren<- c("lightcoral","lightgoldenrod1","cadetblue","burlywood2","palegreen3","goldenrod1")
-Kleuren.disciplines<- c("goldenrod1","burlywood2","azure2","palegreen3","lightcoral","cadetblue","lightgoldenrod1")
+
+# BACKUP Kleuren.disciplines<- c("goldenrod1","burlywood2","azure2","palegreen3","lightcoral","cadetblue","lightgoldenrod1")
+
+# New colorscheme from http://colorbrewer2.org/    qualitative colors
+Kleuren.disciplines<- c("#7fc97f","#beaed4","#fdc086","#ffff99","#386cb0","#f0027f","#bf5b17")
