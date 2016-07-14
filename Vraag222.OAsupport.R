7f8e5ff88316904c95f0c468d1a310e8150b0972
mosart <maurice.vanderfeesten@gmail.com>
2016-07-14 15:48:03 +0200

    changed labels from axes to English

--- Vraag222.OAsupport.R
+++ Vraag222.OAsupport.R
@@ -5,14 +5,14 @@
 win.graph(14,7)
 par(mfrow=c(1,2))
 i=1
-xx <- barplot(support.per.role[,i],xlab="carrieregroep",ylim=c(0,119), 
-              main=paste(colnames(support.per.role)[i]," per carrieregroep",sep=""), 
-              cex.names = 0.7,horiz=F,las=1,ylab="percentage support",col=Kleuren)
+xx <- barplot(support.per.role[,i],xlab="carreer group",ylim=c(0,119), 
+              main=paste(colnames(support.per.role)[i]," per carreer group",sep=""), 
+              cex.names = 0.7,horiz=F,las=1,ylab="% support",col=Kleuren)
 text(x=xx, y=support.per.role[,i],label=paste('n=',respondenten.per.rol,sep=''), 
      pos=3, cex=0.8)
 i=2
-xx <- barplot(support.per.role[,i],xlab="carrieregroep",ylim=c(0,119), 
-              main=paste(colnames(support.per.role)[i]," per carrieregroep",sep=""), 
-              cex.names = 0.7,horiz=F,las=1,ylab="percentage support",col=Kleuren)
+xx <- barplot(support.per.role[,i],xlab="carreer group",ylim=c(0,119), 
+              main=paste(colnames(support.per.role)[i]," per carreer group",sep=""), 
+              cex.names = 0.7,horiz=F,las=1,ylab="% support",col=Kleuren)
 text(x=xx, y=support.per.role[,i],label=paste('n=',respondenten.per.rol,sep=''), 
      pos=3, cex=0.8)
