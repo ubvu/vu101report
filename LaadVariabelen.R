da70960486019eb676f631be2e2c0e5ec97502a2
mosart <maurice.vanderfeesten@gmail.com>
2016-08-02 12:37:53 +0200

    added VU and OECD averages to the discipline section

--- LaadVariabelen.R
+++ LaadVariabelen.R
@@ -16,3 +16,5 @@ Kleuren<- c("lightcoral","lightgoldenrod1","cadetblue","burlywood2","palegreen3"
 
 # New colorscheme from http://colorbrewer2.org/    qualitative colors
 Kleuren.disciplines<- c("#7fc97f","#beaed4","#fdc086","#ffff99","#386cb0","#f0027f","#bf5b17")
+Kleuren.VU<- c("#1691D0")
+Kleuren.OECD<- c("dimgrey")
